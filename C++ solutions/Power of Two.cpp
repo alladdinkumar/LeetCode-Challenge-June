7b19542897265c88e72faa32d1af3c84be49b6ce
alladdinkumar <31990974+alladdinkumar@users.noreply.github.com>
2020-06-09 13:01:03 +0530

    sol

---
+++ C++ solutions/Power of Two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0)
+            return false;
+        if(floor(log2(n))==ceil(log2(n)))
+            return true;
+        else
+            return false;
+        
+    }
+};
